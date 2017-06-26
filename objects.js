ef48200a43b537b27afef10c37da7fbbd3766a58
yungpanko <jrjohns@mit.edu>
2017-06-26 18:23:41 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+let playlist = {
+  'artist': ['song']
+};
+
+function updatePlaylist(playlist, artistName, songName) {
+  playlist[artistName] = songName;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
